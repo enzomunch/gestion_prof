32df5da9b8eece296b471dd0f14bef63c42a961b
Enzo Munch <enzomunchadamou@gmail.com>
2025-03-06 13:16:51 +0100

    initialisation du depot

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace gestion_prof
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labId = new System.Windows.Forms.Label();
+            this.labMdp = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtMdp = new System.Windows.Forms.TextBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labId
+            // 
+            this.labId.AutoSize = true;
+            this.labId.Font = new System.Drawing.Font("Reem Kufi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labId.Location = new System.Drawing.Point(208, 100);
+            this.labId.Name = "labId";
+            this.labId.Size = new System.Drawing.Size(115, 35);
+            this.labId.TabIndex = 0;
+            this.labId.Text = "Identifiant :";
+            // 
+            // labMdp
+            // 
+            this.labMdp.AutoSize = true;
+            this.labMdp.Font = new System.Drawing.Font("Reem Kufi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labMdp.Location = new System.Drawing.Point(189, 135);
+            this.labMdp.Name = "labMdp";
+            this.labMdp.Size = new System.Drawing.Size(134, 35);
+            this.labMdp.TabIndex = 1;
+            this.labMdp.Text = "Mot de passe :";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(343, 113);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtMdp
+            // 
+            this.txtMdp.Location = new System.Drawing.Point(343, 148);
+            this.txtMdp.Name = "txtMdp";
+            this.txtMdp.Size = new System.Drawing.Size(100, 20);
+            this.txtMdp.TabIndex = 3;
+            // 
+            // btnValider
+            // 
+            this.btnValider.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnValider.Location = new System.Drawing.Point(343, 231);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = false;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.BackColor = System.Drawing.Color.LightCoral;
+            this.btnQuitter.Location = new System.Drawing.Point(248, 231);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 5;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(696, 355);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.txtMdp);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.labMdp);
+            this.Controls.Add(this.labId);
+            this.Name = "Form1";
+            this.Text = "Fenetre de connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labId;
+        private System.Windows.Forms.Label labMdp;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtMdp;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Button btnQuitter;
+    }
+}
+
---
+++ central.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace gestion_prof
+{
+    partial class central
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.etudiantSuperviséToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suiviDeStageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.documentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parametresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.etudiantSuperviséToolStripMenuItem,
+            this.suiviDeStageToolStripMenuItem,
+            this.documentsToolStripMenuItem,
+            this.parametresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // etudiantSuperviséToolStripMenuItem
+            // 
+            this.etudiantSuperviséToolStripMenuItem.Name = "etudiantSuperviséToolStripMenuItem";
+            this.etudiantSuperviséToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.etudiantSuperviséToolStripMenuItem.Text = "Etudiant Supervisé";
+            // 
+            // suiviDeStageToolStripMenuItem
+            // 
+            this.suiviDeStageToolStripMenuItem.Name = "suiviDeStageToolStripMenuItem";
+            this.suiviDeStageToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.suiviDeStageToolStripMenuItem.Text = "Suivi de Stage ";
+            // 
+            // documentsToolStripMenuItem
+            // 
+            this.documentsToolStripMenuItem.Name = "documentsToolStripMenuItem";
+            this.documentsToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.documentsToolStripMenuItem.Text = "Documents";
+            // 
+            // parametresToolStripMenuItem
+            // 
+            this.parametresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.parametresToolStripMenuItem.Name = "parametresToolStripMenuItem";
+            this.parametresToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.parametresToolStripMenuItem.Text = "Paramètres";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            // 
+            // central
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "central";
+            this.Text = "Fenetre Principale";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem etudiantSuperviséToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem suiviDeStageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem documentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parametresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+    }
+}

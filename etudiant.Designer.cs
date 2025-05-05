ef3c4db554316fdbfc2b0a2c6a0477baadefaf53
Enzo Munch <enzomunchadamou@gmail.com>
2025-05-05 11:39:12 +0200

    recherche etudiant

--- etudiant.Designer.cs
+++ etudiant.Designer.cs
@@ -30,10 +30,8 @@ namespace gestion_prof
         private void InitializeComponent()
         {
             this.labelNom = new System.Windows.Forms.Label();
-            this.labelPrenom = new System.Windows.Forms.Label();
             this.buttonRechercher = new System.Windows.Forms.Button();
             this.textNom = new System.Windows.Forms.TextBox();
-            this.textPrenom = new System.Windows.Forms.TextBox();
             this.btnQuitter = new System.Windows.Forms.Button();
             this.dataGridViewEtudiant = new System.Windows.Forms.DataGridView();
             this.nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,6 +40,7 @@ namespace gestion_prof
             this.telephone = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.professeur = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.classe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReinitialiser = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEtudiant)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,47 +48,30 @@ namespace gestion_prof
             // 
             this.labelNom.AutoSize = true;
             this.labelNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNom.Location = new System.Drawing.Point(83, 45);
+            this.labelNom.Location = new System.Drawing.Point(97, 62);
             this.labelNom.Name = "labelNom";
             this.labelNom.Size = new System.Drawing.Size(61, 24);
             this.labelNom.TabIndex = 0;
             this.labelNom.Text = "Nom :";
             // 
-            // labelPrenom
-            // 
-            this.labelPrenom.AutoSize = true;
-            this.labelPrenom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPrenom.Location = new System.Drawing.Point(57, 78);
-            this.labelPrenom.Name = "labelPrenom";
-            this.labelPrenom.Size = new System.Drawing.Size(87, 24);
-            this.labelPrenom.TabIndex = 1;
-            this.labelPrenom.Text = "Prénom :";
-            // 
             // buttonRechercher
             // 
-            this.buttonRechercher.Location = new System.Drawing.Point(297, 59);
+            this.buttonRechercher.Location = new System.Drawing.Point(286, 62);
             this.buttonRechercher.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonRechercher.Name = "buttonRechercher";
             this.buttonRechercher.Size = new System.Drawing.Size(87, 28);
             this.buttonRechercher.TabIndex = 2;
-            this.buttonRechercher.Text = "rechercher";
+            this.buttonRechercher.Text = "Rechercher";
             this.buttonRechercher.UseVisualStyleBackColor = true;
+            this.buttonRechercher.Click += new System.EventHandler(this.buttonRechercher_Click);
             // 
             // textNom
             // 
-            this.textNom.Location = new System.Drawing.Point(150, 45);
+            this.textNom.Location = new System.Drawing.Point(164, 65);
             this.textNom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textNom.Name = "textNom";
             this.textNom.Size = new System.Drawing.Size(116, 22);
             this.textNom.TabIndex = 3;
-            // 
-            // textPrenom
-            // 
-            this.textPrenom.Location = new System.Drawing.Point(150, 81);
-            this.textPrenom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textPrenom.Name = "textPrenom";
-            this.textPrenom.Size = new System.Drawing.Size(116, 22);
-            this.textPrenom.TabIndex = 4;
             // 
             // btnQuitter
             // 
@@ -120,7 +102,6 @@ namespace gestion_prof
             this.dataGridViewEtudiant.Name = "dataGridViewEtudiant";
             this.dataGridViewEtudiant.Size = new System.Drawing.Size(814, 411);
             this.dataGridViewEtudiant.TabIndex = 7;
-            this.dataGridViewEtudiant.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEtudiant_CellContentClick);
             // 
             // nom
             // 
@@ -155,6 +136,16 @@ namespace gestion_prof
             this.classe.HeaderText = "Classe";
             this.classe.Name = "classe";
             // 
+            // btnReinitialiser
+            // 
+            this.btnReinitialiser.Location = new System.Drawing.Point(379, 65);
+            this.btnReinitialiser.Name = "btnReinitialiser";
+            this.btnReinitialiser.Size = new System.Drawing.Size(95, 23);
+            this.btnReinitialiser.TabIndex = 8;
+            this.btnReinitialiser.Text = "Réinitialiser";
+            this.btnReinitialiser.UseVisualStyleBackColor = true;
+            this.btnReinitialiser.Click += new System.EventHandler(this.btnReinitialiser_Click);
+            // 
             // etudiant
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
@@ -162,12 +153,11 @@ namespace gestion_prof
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(989, 662);
             this.ControlBox = false;
+            this.Controls.Add(this.btnReinitialiser);
             this.Controls.Add(this.dataGridViewEtudiant);
             this.Controls.Add(this.btnQuitter);
-            this.Controls.Add(this.textPrenom);
             this.Controls.Add(this.textNom);
             this.Controls.Add(this.buttonRechercher);
-            this.Controls.Add(this.labelPrenom);
             this.Controls.Add(this.labelNom);
             this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -185,10 +175,8 @@ namespace gestion_prof
         #endregion
 
         private System.Windows.Forms.Label labelNom;
-        private System.Windows.Forms.Label labelPrenom;
         private System.Windows.Forms.Button buttonRechercher;
         private System.Windows.Forms.TextBox textNom;
-        private System.Windows.Forms.TextBox textPrenom;
         private System.Windows.Forms.Button btnQuitter;
         private System.Windows.Forms.DataGridView dataGridViewEtudiant;
         private System.Windows.Forms.DataGridViewTextBoxColumn nom;
@@ -197,5 +185,6 @@ namespace gestion_prof
         private System.Windows.Forms.DataGridViewTextBoxColumn telephone;
         private System.Windows.Forms.DataGridViewTextBoxColumn professeur;
         private System.Windows.Forms.DataGridViewTextBoxColumn classe;
+        private System.Windows.Forms.Button btnReinitialiser;
     }
 }
--- etudiant.cs
+++ etudiant.cs
@@ -51,9 +51,68 @@ namespace gestion_prof
             }
         }
 
-        private void dataGridViewEtudiant_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void buttonRechercher_Click(object sender, EventArgs e)
         {
+            MySqlConnection connexion = new MySqlConnection("database=gestion_prof; server=localhost; user id =root; mdp=");
+            connexion.Open();
 
+            MySqlCommand cmd = new MySqlCommand("SELECT nomEtudiant, prenomEtudiant, mailEtudiant, telEtudiant, nomProf, nomClasse from etudiants INNER JOIN professeurs ON professeurs.numProf = etudiants.numProf INNER JOIN classes ON classes.numClasse = etudiants.numClasse WHERE nomEtudiant LIKE '%" + textNom.Text + "%'"
+, connexion);
+
+            MySqlDataReader recherche = cmd.ExecuteReader();
+
+            dataGridViewEtudiant.Rows.Clear();
+
+            if (recherche.HasRows)
+            {
+                while (recherche.Read())
+                {
+                    dataGridViewEtudiant.Rows.Add(
+                        recherche[0],
+                        recherche[1],
+                        recherche[2],
+                        recherche[3],
+                        recherche[4],
+                        recherche[5]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Etudiant non trouvé ", "Attention !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            //  Toujours fermer le reader et la connexion
+            recherche.Close();
+            connexion.Close();
+        }
+
+        private void btnReinitialiser_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connexion_etudiants = new MySqlConnection("database = gestion_prof; server = localhost; user id = root; mdp=");
+            try
+            {
+                // Requête pour récupérer la liste des étudiants
+                connexion_etudiants.Open();
+                MySqlCommand cmd_etudiants = new MySqlCommand("SELECT nomEtudiant, prenomEtudiant, mailEtudiant, telEtudiant, nomProf, nomClasse from etudiants INNER JOIN professeurs ON professeurs.numProf = etudiants.numProf INNER JOIN classes ON classes.numClasse = etudiants.numClasse", connexion_etudiants);
+                // Execution de la requete 
+                MySqlDataReader grille_etudiants = cmd_etudiants.ExecuteReader();
+                //
+                dataGridViewEtudiant.Rows.Clear();
+                while (grille_etudiants.Read())
+                {
+                    dataGridViewEtudiant.Rows.Add(
+                        grille_etudiants[0],
+                        grille_etudiants[1],
+                        grille_etudiants[2],
+                        grille_etudiants[3],
+                        grille_etudiants[4],
+                        grille_etudiants[5]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connexion à la base client echoué.", "Attention !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
